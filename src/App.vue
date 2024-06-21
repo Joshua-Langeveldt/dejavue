31ac9c477084fbb8ead64c4ec1804daa6faf3aa7
Joshua Langeveldt <langeveldt20joshua@gmail.com>
2024-06-21 09:32:47 +0200

    Initial Commits

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
+import Friends from './components/Friends.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card,
+    Friends, 
+
   }
 }
 </script>
